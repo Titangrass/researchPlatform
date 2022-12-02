a401910ba92d1781115c9091eb8d075f442672b5
Titangrass <ajaygunness98@gmail.com>
2022-12-02 05:21:48 +0000

    modified:   App/controllers/author.py

--- App/controllers/author.py
+++ App/controllers/author.py
@@ -42,4 +42,31 @@ def getpublicationtree(id):
     if not author:
         return []
     return author.get_publications()
-    
+
+# search that retrieves all publications of the author 
+# as well as the publications of any co-authors of the author, 
+# and the co-authors of their co-authors etc.
+
+# getpublicationtree needs 
+# author root  
+# all publications of this author 
+# and any publication of any coauthor associated wih this author
+# # functionality in get_publications()
+# we have basically a mesh network of authors and publications 
+# each of which may or may not be connected to each other and
+# we need to find the path that links all coauthours to the author in the search
+
+# search for author done by get_author(id)
+# get publications of this author done by get_publications()
+# get publication has a list of authors 
+# for all authors of current Publication 
+#     get their publications and log each subsequent publication's authors 
+
+# need to store:
+# all authors 
+# all publications 
+
+# a binary tree where each node is an author and each has a publication/s
+# we do a bfs that looks at every node and tests 
+# if it has a publication in common with the current list of authors 
+# and we do this search until the list of authors is exhausted
