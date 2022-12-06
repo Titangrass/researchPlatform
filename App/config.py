e8cc8a1d75193be638c99827321d5a0c65190ce5
Titangrass <ajaygunness98@gmail.com>
2022-12-06 08:37:17 +0000

    Changes to be committed: 	modified:   App/config.py     deploying in progress

--- App/config.py
+++ App/config.py
@@ -1,5 +1,8 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+#SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql:///temp-database.db"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
-ENV = "DEVELOPMENT"
+ENV = "PRODUCTION"
+
+
 
