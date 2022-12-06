5017c1fcda3ee644900cfef0c559351efbe60473
Titangrass <ajaygunness98@gmail.com>
2022-12-06 08:45:57 +0000

    Changes to be committed: 	modified:   App/config.py     postgresql not postgres alone

--- App/config.py
+++ App/config.py
@@ -1,5 +1,5 @@
 #SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
-SQLALCHEMY_DATABASE_URI = "postgres://researchplatform_postgresql_user:ZkJ0c9EkuaYDUjMFuveNLY5sYBM0iI3X@dpg-ce7g14g2i3mre1lbkr00-a/researchplatform_postgresql"
+SQLALCHEMY_DATABASE_URI = "postgresql://researchplatform_postgresql_user:ZkJ0c9EkuaYDUjMFuveNLY5sYBM0iI3X@dpg-ce7g14g2i3mre1lbkr00-a/researchplatform_postgresql"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "PRODUCTION"
